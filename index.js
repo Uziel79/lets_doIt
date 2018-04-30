c20ff9bd1865c59f8bc41a016e21e994947555a5
Armando Del Col <armandodelcol@yahoo.com.br>
2018-04-30 13:48:03 -0300

    primeiro commit do projeto

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const nunjucks = require('nunjucks');
+
+const app = express();
+
+// DEFININDO A VIEW ENGINE PARA EXTENSÃO DO NUNJUCKS
+app.set('view engine', 'njk');
+// DEFINDO O CAMINHO ORIGINAL DA PASTA VIEWS DO PROJETO
+app.set('views', path.join('app', 'views'));
+// DEFINDO O CAMINHO DAS VIEWS DO NUNJUCKS NA APLICAÇÃO
+nunjucks.configure(app.get('views'), {
+  autoescape: true,
+  express: app,
+});
+// DEFININDO O CAMINHO DA PASTA COM ARQUIVOS PUBLIC(CSS, JS, IMGS...)
+app.use(express.static(path.resolve('app', 'public')));
+
+app.listen(3000, () => console.log('Server is running!'));
